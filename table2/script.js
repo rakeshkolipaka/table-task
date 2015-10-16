e2672c2bf8baaf5a2c646bf7f692c2e1d7fa86f0
rakeshkolipaka <rakeshkolipaka91@gmail.com>
2015-10-16 17:30:22 -0400

    Used Json Server

--- table2/script.js
+++ table2/script.js
@@ -1,11 +1,10 @@
 function loadData(){
-   console.log("loadData");
   $.ajax('http://localhost:3000/comments', {
     method:'GET'
   }).then(function(data){
     console.log("loadData in ajax gettop");
       for(var i=0;i<data.length;i++){
-      $("#formContainer").append("<div class='row datarow'><div class='col-md-1 col-sm-1 col-xs-2'><span>"+data[i].userId+"</span></div><div class='col-md-1 col-sm-1 col-xs-2'><span>"+data[i].id+"</span></div><div class='col-md-2 col-sm-2 col-xs-8'><span>"+data[i].title+"</span></div><div class='col-md-6 col-sm-6 col-xs-12'><span>"+data[i].body+"</span></div><div class='col-md-2 col-sm-2 col-xs-2'><span><button type='button' class='btn btn-warning' id='edit1' data-toggle='modal' data-target='#myModal' onclick='control("+i+")' ><span class='glyphicon glyphicon-pencil'></span></button>"+"<button type='button' class='btn btn-danger' id='delet' onclick='control1("+data[i].id+")' data-toggle='tooltip' data-placement='top' title='This will Delet the data' ><span class='glyphicon glyphicon-trash'>"+"</span></button></span></div></div>");  
+      $("#formContainer").append("<div class='row datarow'><div class='col-md-1 col-sm-1 col-xs-2'><span>"+data[i].userId+"</span></div><div class='col-md-1 col-sm-1 col-xs-2' data-toggle='modal' data-target='#myModal2' onclick='control2("+i+")'><span>"+data[i].id+"</span></div><div class='col-md-2 col-sm-2 col-xs-8'><span>"+data[i].title+"</span></div><div class='col-md-6 col-sm-6 col-xs-12'><span>"+data[i].body+"</span></div><div class='col-md-2 col-sm-2 col-xs-2'><span><button type='button' class='btn btn-warning' id='edit1' data-toggle='modal' data-target='#myModal' onclick='control("+i+")' ><span class='glyphicon glyphicon-pencil'></span></button>"+"<button type='button' class='btn btn-danger' id='delet' onclick='control1("+data[i].id+")' data-toggle='tooltip' data-placement='top' title='This will Delet the data' ><span class='glyphicon glyphicon-trash'>"+"</span></button></span></div></div>");  
       //$("#table1").append("<tr><td>"+data[i].userId+"</td><td>"+data[i].id+"</td><td>"+data[i].title+"</td><td class='col-md-3'>"+data[i].body+"</td><td><button type='button' class='btn btn-warning' id='edit1' data-toggle='modal' data-target='#myModal' onclick='control("+data[i].id+")' ><span class='glyphicon glyphicon-pencil'></span></button><button type='button' class='btn btn-danger' id='delet' onclick='control1("+data[i].id+")' data-toggle='tooltip' data-placement='top' title='This will Delet the data' ><span class='glyphicon glyphicon-trash'>"+"</span></button></td></tr>");  
   }
   });
@@ -54,7 +53,7 @@ function control(lo){
         method:'GET'
       }).then(function(data){
 
-          console.log(data[j].userId);
+          
         document.getElementById("uidNew").innerHTML=data[j].userId;
         document.getElementById("tNew").innerHTML=data[j].title;
         document.getElementById("bodyNew").innerHTML=data[j].body;
@@ -76,20 +75,52 @@ function control(lo){
           });
         }
         else{
-          alert("every feild required");
+          alert("every feild required");654
         }
       });
 
 });
-
-
-
-
-
 }
+function control2(k){
+
+  $.ajax('http://localhost:3000/comments', {
+        method:'GET'
+      }).then(function(data){
+
+          console.log('log:'+k+"="+data[k].id);
+        document.getElementById("userIdEdit").innerHTML=data[k].userId;
+        document.getElementById("titleEdit").innerHTML=data[k].title;
+        document.getElementById("bodyEdit").innerHTML=data[k].body;
+        $("#submit1").click(function(){ 
+            var g=$("#userIdEdit").val();
+            var h=$("#titleEdit").val();
+            var l=$("#bodyEdit").val();
+        if(g!=""&&h!=""&&l!=""){
+          $.ajax('http://localhost:3000/comments/'+data[k].id+'', {
+            method: 'PATCH',
+            data: {
+              title: h,
+              userId:g,
+              body:l 
+            }
+          }).then(function(data) {
+            $(".datarow").remove();
+           loadData();
+          });
+        }
+        else{
+          alert("every feild required");654
+        }
+      });
+});
+}
+
 $("#reload").click(function(){
   $("#editDetails")[0].reset();
   });
 $("#reload1").click(function(){
-  $("#newDetails")[0].reset();
+  $("#addNewRow")[0].reset();
+  });
+$("#reload2").click(function(){
+  $("#editD")[0].reset();
   });
